aa8de96df451bd8e24bacd15a1cf79fb2926efea
Chau Vo <minhchau273@gmail.com>
2016-07-14 14:23:49 +0700

    [f] Milestone 2: Build the Main Screen

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+  @IBOutlet weak var photoImageView: UIImageView!
+
+  var initialPhotoCenter: CGPoint!
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
+  }
+
+  @IBAction func onPhotoPanGesture(sender: UIPanGestureRecognizer) {
+    let state = sender.state
+    let translation = sender.translationInView(view)
+
+    switch state {
+        case .Began:
+      initialPhotoCenter = photoImageView.center
+    case .Changed:
+      photoImageView.center = CGPoint(x: initialPhotoCenter.x + translation.x, y: initialPhotoCenter.y)
+    default:
+      break
+    }
   }
 
 }
