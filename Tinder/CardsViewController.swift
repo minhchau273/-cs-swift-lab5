c1639dc67855fc91dac1e5a79f1956cb3b0b753d
Chau Vo <minhchau273@gmail.com>
2016-07-14 14:12:55 +0700

    [f] Milestone 1: Setup

---
+++ Tinder/CardsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CardsViewController.swift
+//  Tinder
+//
+//  Created by Chau Vo on 7/14/16.
+//  Copyright © 2016 Chau Vo. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+}
